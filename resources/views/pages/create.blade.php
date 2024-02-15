4939bc84e3a8bd717a354412cb0f130fbaa96b71
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-15 16:37:11 +0100

    add: form

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -5,4 +5,19 @@
 @section('content')
 <h1>Add new comic</h1>
 
+<form action="{{ route('route.store') }}" method="POST">
+
+    @csrf
+    @method('POST')
+
+    <label for="title">Title</label>
+    <input type="text" name="title" id="title">
+    <br>
+    <label for="author">Author</label>
+    <input type="text" name="author" id="author">
+    <br>
+    <br>
+    <input type="submit" value="CREATE">
+</form>
+
 @endsection
