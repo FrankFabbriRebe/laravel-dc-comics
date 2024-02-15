75177114eb2245a65b4f5d0c5ffba4455bfd14cc
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-15 15:08:30 +0100

    create: ComicTableSeeder

---
+++ database/seeders/ComicTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+// import model
+use App\Models\Comic;
+
+class ComicTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
