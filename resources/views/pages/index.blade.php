0324102126b9e91d4c39e4640ea408ef4f008725
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-15 16:21:58 +0100

    add: link to create page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,11 +3,16 @@
 <title>Home</title>
 @endsection
 @section('content')
+
 <h1>Comic list</h1>
+
+<a href="{{route('route.create')}}">Click here for add a new comic</a>
+
 @foreach ($comics as $comic)
 <li>
     <strong>Comic title: </strong>{{ $comic -> title}}
 </li>
-<a href="{{ route('route.show', $comic -> id) }}">Clik her for more details</a>
+<a href="{{ route('route.show', $comic -> id) }}">Clik here for more details</a>
 @endforeach
+
 @endsection
