5dd982541984ba60af19f380a68b7e129e4fbf4a
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-15 15:28:15 +0100

    amend: model name

--- database/migrations/2024_02_15_135440_create_comics_table.php
+++ database/migrations/2024_02_15_135440_create_comics_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create('comic', function (Blueprint $table) {
+        Schema::create('comics', function (Blueprint $table) {
             $table->id();
 
             $table->string('title');
@@ -29,6 +29,6 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::dropIfExists('comic');
+        Schema::dropIfExists('comics');
     }
 };
