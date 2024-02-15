eada22f2b1caa03a71e254926b2f84f1add6a6ff
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-15 15:47:41 +0100

    add: variable and compact data

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -16,69 +16,39 @@ class MainController extends Controller
         $comics = Comic::all();
 
         return view('pages.index', compact('comics'));
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     // public function create()
     // {
     //     //
     // }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     // public function store(Request $request)
     // {
     //     //
     // }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    // public function show($id)
-    // {
+    public function show($id)
+    {
 
-    // }
+        $comics = Comic::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+        return view('pages.show', compact('comics'));
+
+    }
+
     // public function edit($id)
     // {
     //     //
     // }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     // public function update(Request $request, $id)
     // {
     //     //
     // }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     // public function destroy($id)
     // {
     //     //
