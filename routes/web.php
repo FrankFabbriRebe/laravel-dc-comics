b66300a11e56c292d5113c53c1de5ea03c9da8f9
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-15 17:00:37 +0100

    change: address for store page

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,12 @@ use App\Http\Controllers\MainController;
 // route.index
 Route::get('/', [MainController::class, 'index'])->name('route.index');
 
+
 // route.create
 Route::get('/create', [MainController::class, 'create'])->name('route.create');
 
 // route.store
-Route::post('/users', [MainController::class, 'store'])->name('route.store');
+Route::post('/', [MainController::class, 'store'])->name('route.store');
 
 // route.show
 Route::get('/show{id}', [MainController::class, 'show'])->name('route.show');
