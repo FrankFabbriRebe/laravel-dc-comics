b18e4e3d025ed43740371ac7093349f66b5f56e3
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-15 15:47:03 +0100

    create: route for method show

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,9 @@ use Illuminate\Support\Facades\Route;
 // import MainController
 use App\Http\Controllers\MainController;
 
+// route.index
 Route::get('/', [MainController::class, 'index'])->name('route.index');
+
+// route.show
+Route::get('/show{id}', [MainController::class, 'show'])->name('route.show');
+
