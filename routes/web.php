ef74ded655b7d9ed32cbb131336fcbd4d30e2064
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-15 12:59:32 +0100

    connection: MainController

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// import MainController
+use App\Http\Controllers\MainController;
 
-    return view('pages.index');
-});
+Route::get('/', [MainController::class, 'index']);
