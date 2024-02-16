6a4e34445044e46adbc084623efb4e6e23c9cbc9
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-16 15:08:18 +0100

    add: form

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,16 +5,35 @@
 @section('content')
 
 <h1>Comic list</h1>
+<br>
+<br>
 
 <h2>Comics: {{ count($comics) }}</h2>
+<br>
+<br>
 
+<!-- link to get create new comic page -->
 <a href="{{route('route.create')}}">Click here for add a new comic</a>
+<br>
+<br>
 
 @foreach ($comics as $comic)
+
 <li>
     <strong>Comic title: </strong>{{ $comic -> title}}
 </li>
+
+<!-- link to get show page -->
 <a href="{{ route('route.show', $comic -> id) }}">Clik here for more details</a>
+
+<!-- to delete element -->
+<form>
+    <input type="submit" value="X">
+</form>
+
+<br>
+<br>
+
 @endforeach
 
 @endsection
