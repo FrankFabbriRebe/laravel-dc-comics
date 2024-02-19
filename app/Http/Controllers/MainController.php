cc30b919482084f5d76ebada655f4c77c1531af3
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-19 15:20:18 +0100

    add: connection method store with validation

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -29,7 +29,15 @@ class MainController extends Controller
 
     }
 
-    public function store(Request $request)
+    public function store(@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif $request)
     {
         $data = $request->all();
 
