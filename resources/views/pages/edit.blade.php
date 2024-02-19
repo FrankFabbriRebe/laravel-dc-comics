2084b3c8248fb82c57b8310df2ff75ca910503da
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-19 15:16:41 +0100

    add: error messages

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -5,6 +5,16 @@
 @section('content')
 <h1>edit comic</h1>
 
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
 <form action="{{ route('route.update', $comic -> id) }}" method="POST">
 
     @csrf
